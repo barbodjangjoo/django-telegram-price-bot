88981af564a2887ff878e152990f56f2b350eaa8
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2026-01-28 21:32:12 +0330

    bugfixed

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -2,6 +2,7 @@ import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 from bot.handlers.start import start
 from bot.handlers.my_status import my_status
+from bot.handlers.ref_summary import ref_summary
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
@@ -9,6 +10,6 @@ def start_bot():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("my_status", my_status))
-    app.add_handler(CommandHandler(("ref_summary", my_status)))
+    app.add_handler(CommandHandler("ref_summary", ref_summary))
     print("Bot is running...")
     app.run_polling()
